905b24620558d39373ccedba12717685e7e4cf2d
jusukarneh <135680474+jusukarneh@users.noreply.github.com>
2023-09-26 18:40:14 -0400

    images

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,11 @@ const homeRoutes =require('./homeRoutes');
 router.use('/',homeRoutes)
 router.use('/api', apiRoutes);
 
+//html routes
+router.use('/', homeRoutes);
+
+//api routes 
+router.use('/api',apiRoutes);
+
 module.exports = router;
+
