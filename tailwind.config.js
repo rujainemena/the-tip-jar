a6112bf27d76e67fb73d8796eb2456fa65bb29c8
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-10-01 17:50:49 -0400

    front end updates to homepage.handlebars

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/layouts/main.handlebars"],
+  content: ["./views/layouts/main.handlebars", "./views/layouts/homepage.handlebars"],
   theme: {
     extend: {},
   },
